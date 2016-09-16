e9b7cfbe32122925b0312ba84e01b192adf0eb4c
Jesse Bounds <jesse@rebounds.net>
2016-09-16 16:50:00 -0700

    Integrate style-spec 'auto' property for symbol pitch and rotation
    This updates the symbol style layer so that it uses the correct
    values (viewport or map) when the auto value is set explicitly
    or by default for the properties that support it.
    
    text-pitch-alignment continues to parrot text-rotation-alignment but
    now also does so if its value is explicitly set to auto.
    
    This also updates the mapbox-gl-style-spec to use the latest from
    the mapbox-gl-style-spec symbol-complex-default branch and includes
    a rebuilt v8.min.json.

--- js/style/style_layer/symbol_style_layer.js
+++ js/style/style_layer/symbol_style_layer.js
@@ -12,19 +12,28 @@ module.exports = SymbolStyleLayer;
 SymbolStyleLayer.prototype = util.inherit(StyleLayer, {
 
     getLayoutValue: function(name, globalProperties, featureProperties) {
-        if (name === 'text-rotation-alignment' &&
-                this.getLayoutValue('symbol-placement', globalProperties, featureProperties) === 'line' &&
-                !this.getLayoutProperty('text-rotation-alignment')) {
-            return 'map';
-        } else if (name === 'icon-rotation-alignment' &&
-                this.getLayoutValue('symbol-placement', globalProperties, featureProperties) === 'line' &&
-                !this.getLayoutProperty('icon-rotation-alignment')) {
-            return 'map';
-        // If unspecified `text-pitch-alignment` inherits `text-rotation-alignment`
-        } else if (name === 'text-pitch-alignment' && !this.getLayoutProperty('text-pitch-alignment')) {
+        if (name === 'text-rotation-alignment' && !this.getLayoutProperty('text-rotation-alignment')) {
+            return this.defaultLayoutType(globalProperties, featureProperties);
+        } else if (name === 'icon-rotation-alignment' && !this.getLayoutProperty('icon-rotation-alignment')) {
+            return this.defaultLayoutType(globalProperties, featureProperties);
+        // If unspecified or auto `text-pitch-alignment` inherits `text-rotation-alignment`
+        } else if (name === 'text-pitch-alignment' &&
+                  (!this.getLayoutProperty('text-pitch-alignment') || this.getLayoutProperty('text-pitch-alignment') === 'auto')) {
             return this.getLayoutValue('text-rotation-alignment');
         } else {
-            return StyleLayer.prototype.getLayoutValue.apply(this, arguments);
+            var layoutValue = StyleLayer.prototype.getLayoutValue.apply(this, arguments);
+            if (layoutValue === 'auto') {
+                return this.defaultLayoutType(globalProperties, featureProperties);
+            }
+            return layoutValue;
+        }
+    },
+
+    defaultLayoutType: function(globalProperties, featureProperties) {
+        if (this.getLayoutValue('symbol-placement', globalProperties, featureProperties) === 'line') {
+            return 'map';
+        } else {
+            return 'viewport';
         }
     }
 
